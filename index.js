3d9c3e81a9cf3c8de1bd6b9dc2d149080331e485
daniel.velez <daniel.velez@lightware.com>
2024-11-22 22:31:57 +0100

    Fix ajv call, resource reference, README, package.json..

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-// Get the path to the ajv-cli executable
-const ajvPath = path.join(__dirname, 'node_modules', '.bin', 'ajv');
-
 // Forward all command-line arguments to ajv-cli
 const args = process.argv.slice(2);
 const outputArgIndex = args.indexOf("-o");
@@ -16,14 +13,14 @@ if(outputArgIndex === -1 || !outputFile) {
     process.exit(1);
 }
 // Spawn ajv-cli process
-const ajvProcess = spawn(ajvPath, ["compile", ...args], { stdio: 'inherit' });
+const ajvProcess = spawn('ajv', ["compile", ...args], { stdio: 'inherit' });
 
 // Handle process exit
 ajvProcess.on('exit', (code) => {
     if(code === 0) {
         // Compile succeeded, wrap the output file
         const outputData = fs.readFileSync(outputFile, 'utf-8');
-        const snippet = fs.readFileSync("resources/beforeSnippet.js", 'utf-8');
+        const snippet = fs.readFileSync(path.join(__dirname,"resources","beforeSnippet.js"), 'utf-8');
 
         fs.writeFileSync(outputFile, snippet+outputData, 'utf-8');
         fs.chmodSync(outputFile, "755");
